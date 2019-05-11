0236062d39f367f24f4c284d3e447dc1c2640292
mountainflo <tobusch@hm.edu>
2019-05-11 23:43:12 +0200

    [#19] add todo comments

--- backend/algorithmnHandler.go
+++ backend/algorithmnHandler.go
@@ -14,9 +14,12 @@ func HandleAlgorithmCalls(receivedData GuiRequestData) JsonResponse {
 
 	graphObject := buildGraphObjectFromJSON(receivedData)
 
+	//TODO @SDouglas3 build intolerance graph here from graphObject.
+	// Outsource logic in separate function (for details see issue #21)
+
 	if receivedData.Settings.Algorithm == BASIC_GREEDY {
 		resultGraph := algorithms.BasicGreedy(graphObject)
-		fmt.Println("[DEBUG] generated result graph with Basic Greedy Algorithmn ", resultGraph)
+		fmt.Println("[DEBUG] generated result graph with Basic Greedy Algorithm ", resultGraph)
 
 		return JsonResponse{true, resultGraph}
 	} else {
@@ -28,5 +31,8 @@ func HandleAlgorithmCalls(receivedData GuiRequestData) JsonResponse {
 Build a Graph-Object from the received JSON-Data
 */
 func buildGraphObjectFromJSON(data GuiRequestData) adjGraph.Graph {
+
+	//TODO @mike-la build graph object here (for details see issue #20)
+
 	return adjGraph.NewGraphAdjMat(2)
 }
--- backend/algorithms/basicGreedy.go
+++ backend/algorithms/basicGreedy.go
@@ -10,5 +10,8 @@ Calculate the optimization of the traffic lights with basic Greedy algorithm
 */
 func BasicGreedy(graphData adjGraph.Graph) adjGraph.Graph {
 	fmt.Println("[INFO] Called BasicGreedy Algorithmn")
+
+	//TODO implement basic greedy algorithm. (for details see issue #12 and issue #3)
+
 	return adjGraph.NewGraphAdjMat(2)
 }

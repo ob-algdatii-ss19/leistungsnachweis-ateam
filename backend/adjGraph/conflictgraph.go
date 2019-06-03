e4d1bdf234cf7695f0f12f2cb437d402e49d605c
SDouglas3 <s.douglas0@hm.edu>
2019-06-03 10:39:53 +0200

    Herauswurf der ursprünglichen Funktion

--- backend/adjGraph/conflictgraph.go
+++ backend/adjGraph/conflictgraph.go
@@ -68,81 +68,9 @@ func MakeConflictGraphOutOfConnectionGraph(connectionGraph AdjMat) ReturnType {
 						}
 					}
 				} else {
+
 					if i != 0 && j != 0 && i != 6 && j != 6 && i != 12 && j != 12 && i != 15 && j != 15 { //Abfangen der Rechtsabbieger
-						//if i != 1 && j != 1 && i != 7 && j != 7 && i != 13 && j != 13 && i != 16 && j != 16 { //Abfangen der Rechtsabbieger
-						if !((i == 2 && j == 11) || (i == 5 && j == 17)) { //kein Linksabbieger Paar 2&11; 5&17
-							if Entries[i].from != Entries[j].from {
-								if Entries[i].to != Entries[j].to {
-									conflictGraph.UAddEdge(Node(i+1), Node(j+1))
-									//conflictGraph.AddEdge(Node(j),Node(i))  //der Graph ist ja Symmetrisch
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-	tmp := ReturnType{Entries, conflictGraph}
-	return tmp
-}
-
-func MakeConflictGraphOutOfConnectionGraph2(connectionGraph AdjMat) ReturnType {
-	var Entries []trafficEntry = makeList(connectionGraph)
-	var conflictGraph UGraph = NewUGraph(20)
-	for i := 0; i < 20; i++ {
-		for j := i + 1; j < 20; j++ {
-			if Entries[i].isTrue && Entries[j].isTrue {
-				if i%5 == 3 { //Pruefung auf Fu-g'nger bei i
-					if j%5 != 3 && j%5 != 4 { //Pruefung auf weiteren Fussgaenger- zwei Fussgaenger koennen zueinander nicht im Konflikt stehen
-						if Entries[i+1].isTrue { //mit Fussgaengerinsel
-							if Entries[i].from == Entries[j].from {
-								conflictGraph.UAddEdge(Node(i+1), Node(j+1))
-							}
-						} else { //ohne Fu-g'nger Insel
-							if Entries[i].from == Entries[j].to {
-								conflictGraph.UAddEdge(Node(i+1), Node(j+1))
-							}
-							if Entries[i].from == Entries[j].from {
-								conflictGraph.UAddEdge(Node(i+1), Node(j+1))
-							}
-						}
-					}
-				} else if j%5 == 3 { //Prufung der Fussg'nger bei j
-					if i%5 != 3 && i%5 != 4 { //Pruefung auf weiteren Fussgaenger- zwei Fussgaenger koennen zueinander nicht im Konflikt stehen
-						if Entries[j+1].isTrue { //mit Fussgaengerinsel
-							if Entries[i].from == Entries[j].from {
-								conflictGraph.UAddEdge(Node(i+1), Node(j+1))
-							}
-						} else { //Ohne Fussgaengerinsel
-							if Entries[j].from == Entries[i].to {
-								conflictGraph.UAddEdge(Node(j+1), Node(i+1))
-							}
-							if Entries[j].from == Entries[i].from {
-								conflictGraph.UAddEdge(Node(i+1), Node(j+1))
-							}
-
-						}
-					}
-				} else if i%5 == 4 { //Falls Fussgaengerinsel ueberhaupt vorhanden
-					if j%5 != 3 && j%5 != 4 { //Pruefung auf weiteren Fussgaenger- zwei Fussgaenger koennen zueinander nicht im Konflikt stehen
-						if Entries[i].isTrue { //Falls Fussgaengerinsel ueberhaupt vorhanden
-							if Entries[i].from == Entries[j].to {
-								conflictGraph.UAddEdge(Node(i+1), Node(j+1))
-							}
-						}
-					}
-				} else if j%5 == 4 { //Falls Fussgaengerinsel ueberhaupt vorhanden
-					if i%5 != 3 && i%5 != 4 { //Pruefung auf weiteren Fussgaenger- zwei Fussgaenger koennen zueinander nicht im Konflikt stehen
-						if Entries[j].isTrue {
-							if Entries[j].from == Entries[i].to {
-								conflictGraph.UAddEdge(Node(i+1), Node(j+1))
-							}
-						}
-					}
-				} else {
-					//if i != 0 && j != 0 && i != 6 && j != 6 && i != 12 && j != 12 && i != 15 && j != 15 { //Abfangen der Rechtsabbieger
-					if !((i == 0 || i == 6 || i == 12 || i == 15) && (j == 0 || j == 6 || j == 12 || j == 15)) { //Zwei Rechtsabbieger stehen nicht im Konflikt
+						//if !((i == 0 || i == 6 || i == 12 || i == 15) && (j == 0 || j == 6 || j == 12 || j == 15)) { //Zwei Rechtsabbieger stehen nicht im Konflikt
 						if !((i == 2 && j == 11) || (i == 5 && j == 17)) { //kein Linksabbieger Paar 2&11; 5&17
 							if Entries[i].from != Entries[j].from {
 								if !((Entries[i].from == Entries[j].to) && (Entries[i].to == Entries[j].from)) {
@@ -151,6 +79,9 @@ func MakeConflictGraphOutOfConnectionGraph2(connectionGraph AdjMat) ReturnType {
 							}
 						}
 					}
+				}
+				if Entries[i].to == Entries[j].to {
+					conflictGraph.UAddEdge(Node(i+1), Node(j+1))
 				}
 			}
 		}
@@ -179,3 +110,19 @@ func makeList(matrix AdjMat) []trafficEntry {
 	}
 	return Entries
 }
+
+/*func MakeCompatibilityGraph(conflictReturn ReturnType) ReturnType{
+	var compGraph UGraph = NewUGraph(20)
+	for i:=0;i< len(conflictReturn.Entries);i++{
+		for j:=i+1; j < len(conflictReturn.Entries) ; j++{
+			if conflictReturn.Entries[i].isTrue && conflictReturn.Entries[j].isTrue{
+				//if conflictReturn.UGraph[i+1][j+1] wenn es eine kante hat -> Kriegt keine Kante
+				//else kriegt Kante
+
+			}
+
+		}
+	}
+	tmp := ReturnType{conflictReturn.Entries, compGraph}
+	return tmp
+}*/
--- backend/adjGraph/conflictgraph_test.go
+++ backend/adjGraph/conflictgraph_test.go
@@ -5,246 +5,7 @@ import (
 	"testing"
 )
 
-func TestMakeConflictGraphOutOfConnectionGraph(t *testing.T) {
-	//Test1 : Nur Rechtsabbieger, keine Fußgänger
-	//Input : O,C,G,K
-
-	wantedEdges1 := []Edge{
-		{4, 1},
-		{1, 2},
-		{2, 3},
-		{3, 4},
-	}
-
-	g := NewGraphAdjMat(6)
-	for _, edge := range wantedEdges1 {
-		g.AddEdge(edge.from, edge.to)
-	}
-
-	//	gotEdges := g.Edges()
-	m := MakeConflictGraphOutOfConnectionGraph(g)
-	gotEdges := m.UGraph.UEdges()
-
-	if len(gotEdges) != 0 {
-		t.Errorf("Rechtsabbieger sollten keinen Konflikt aufrufen")
-	}
-}
-
-func TestMakeConflictGraphOutOfConnectionGraph2(t *testing.T) {
-	//Test2 : Nur Linksabbieger, keine Fußgänger
-	//Input : M,A,E,I
-
-	wantedEdges1 := []Edge{
-		{4, 3},
-		{1, 4},
-		{2, 1},
-		{3, 2},
-	}
-
-	g := NewGraphAdjMat(6)
-	for _, edge := range wantedEdges1 {
-		g.AddEdge(edge.from, edge.to)
-	}
-
-	//	gotEdges := g.Edges()
-	m := MakeConflictGraphOutOfConnectionGraph(g)
-	gotEdges := m.UGraph.UEdges()
-
-	expected := []Edge{{3, 6},
-		{3, 18},
-		{6, 3},
-		{6, 12},
-		{12, 6},
-		{12, 18},
-		{18, 3},
-		{18, 12}}
-	for index, element := range gotEdges {
-		if expected[index] != element {
-			t.Errorf("Unexpected Node")
-		}
-	}
-}
-
-func TestMakeConflictGraphOutOfConnectionGraph3(t *testing.T) {
-	//Test3 : keine Fußgänger, zwei Rechtsabbieger,  1 Gerade aus Fahrer, 2 Linksabbieger
-	//Input : E,G,I,B,C
-
-	wantedEdges1 := []Edge{
-		{1, 2},
-		{1, 3},
-		{2, 1},
-		{2, 3},
-		{3, 2},
-	}
-
-	g := NewGraphAdjMat(6)
-	for _, edge := range wantedEdges1 {
-		g.AddEdge(edge.from, edge.to)
-	}
-
-	//	gotEdges := g.Edges()
-	m := MakeConflictGraphOutOfConnectionGraph(g)
-	gotEdges := m.UGraph.UEdges()
-
-	expected := []Edge{{2, 6}, {2, 12}, {6, 2}, {6, 12}, {12, 2}, {12, 6}}
-
-	for index, element := range gotEdges {
-		if expected[index] != element {
-			t.Errorf("Wrong Node")
-		}
-	}
-}
-
-/*2 ABC IJK
-6 EFG ABC
-12 IJK EFG
-*/
-
-func TestMakeConflictGraphOutOfConnectionGraph5(t *testing.T) {
-	//Test5 : keine Fußgänger, nur geradeausfahrer
-	//Input : B,F,J,N
-
-	wantedEdges1 := []Edge{
-		{1, 3},
-		{2, 4},
-		{3, 1},
-		{4, 2},
-	}
-
-	g := NewGraphAdjMat(6)
-	for _, edge := range wantedEdges1 {
-		g.AddEdge(edge.from, edge.to)
-	}
-
-	//	gotEdges := g.Edges()
-	m := MakeConflictGraphOutOfConnectionGraph(g)
-	gotEdges := m.UGraph.UEdges()
-
-	expected := []Edge{{2, 8},
-		{2, 11},
-		{2, 17},
-		{8, 2},
-		{8, 11},
-		{8, 17},
-		{11, 2},
-		{11, 8},
-		{11, 17},
-		{17, 2},
-		{17, 8},
-		{17, 11}}
-
-	for index, element := range gotEdges {
-		if expected[index] != element {
-			t.Errorf("Wrong Node")
-		}
-	}
-}
-
-func TestMakeConflictGraphOutOfConnectionGraph6(t *testing.T) {
-	//Test5 : Fußgänger ohne insel,  rechtsabbieger
-	//Input : B,F,J,N
-
-	wantedEdges1 := []Edge{
-		{4, 1},
-		{1, 2},
-		{2, 3},
-		{3, 4},
-		{1, 5},
-		{2, 5},
-		{3, 5},
-		{4, 5},
-	}
-
-	g := NewGraphAdjMat(6)
-	for _, edge := range wantedEdges1 {
-		g.AddEdge(edge.from, edge.to)
-	}
-
-	//	gotEdges := g.Edges()
-	m := MakeConflictGraphOutOfConnectionGraph(g)
-	gotEdges := m.UGraph.UEdges()
-
-	expected := []Edge{{1, 4},
-		{1, 9},
-		{4, 1},
-		{4, 16},
-		{7, 9},
-		{7, 14},
-		{9, 1},
-		{9, 7},
-		{13, 14},
-		{13, 19},
-		{14, 7},
-		{14, 13},
-		{16, 4},
-		{16, 19},
-		{19, 13},
-		{19, 16}}
-
-	for index, element := range gotEdges {
-		if expected[index] != element {
-			t.Errorf("Wrong Node")
-		}
-	}
-
-	for _, element := range gotEdges {
-		fmt.Println(element)
-	}
-}
-
-func TestMakeConflictGraphOutOfConnectionGraph7(t *testing.T) {
-	//Test5 : Fußgänger mit insel,  rechtsabbieger
-	//Input : B,F,J,N
-
-	wantedEdges1 := []Edge{
-		{4, 1},
-		{1, 2},
-		{2, 3},
-		{3, 4},
-		{1, 5},
-		{2, 5},
-		{3, 5},
-		{4, 5},
-		{1, 6},
-		{2, 6},
-		{3, 6},
-		{4, 6},
-	}
-
-	g := NewGraphAdjMat(6)
-	for _, edge := range wantedEdges1 {
-		g.AddEdge(edge.from, edge.to)
-	}
-
-	//	gotEdges := g.Edges()
-	m := MakeConflictGraphOutOfConnectionGraph(g)
-	gotEdges := m.UGraph.UEdges()
-
-	expected := []Edge{{1, 4},
-		{1, 10},
-		{4, 1},
-		{5, 16},
-		{7, 9},
-		{7, 15},
-		{9, 7},
-		{10, 1},
-		{13, 14},
-		{13, 20},
-		{14, 13},
-		{15, 7},
-		{16, 5},
-		{16, 19},
-		{19, 16},
-		{20, 13}}
-
-	for index, element := range gotEdges {
-		if expected[index] != element {
-			t.Errorf("Wrong Node")
-		}
-	}
-}
-
-func ExampleMakeConflictGraphOutOfConnectionGraph8() {
+func TestMakeConflictGraphOutOfConnectionGraph8(t *testing.T) {
 	//Test8 : no pedestrian, B and M are in conflict
 	// two lanes end at the same node
 	//Input : B, C, M
@@ -332,7 +93,7 @@ func TestMakeConflictGraphOutOfConnectionGraph_2(t *testing.T) {
 	}
 
 	//	gotEdges := g.Edges()
-	m := MakeConflictGraphOutOfConnectionGraph2(g)
+	m := MakeConflictGraphOutOfConnectionGraph(g)
 	gotEdges := m.UGraph.UEdges()
 
 	if len(gotEdges) != 0 {
@@ -357,7 +118,7 @@ func TestMakeConflictGraphOutOfConnectionGraph_2_2(t *testing.T) {
 	}
 
 	//	gotEdges := g.Edges()
-	m := MakeConflictGraphOutOfConnectionGraph2(g)
+	m := MakeConflictGraphOutOfConnectionGraph(g)
 	gotEdges := m.UGraph.UEdges()
 
 	expected := []Edge{{3, 6},
@@ -393,7 +154,7 @@ func TestMakeConflictGraphOutOfConnectionGraph_2_3(t *testing.T) {
 	}
 
 	//	gotEdges := g.Edges()
-	m := MakeConflictGraphOutOfConnectionGraph2(g)
+	m := MakeConflictGraphOutOfConnectionGraph(g)
 	gotEdges := m.UGraph.UEdges()
 
 	expected := []Edge{{1, 12},
@@ -436,7 +197,7 @@ func TestMakeConflictGraphOutOfConnectionGraph_2_5(t *testing.T) {
 	}
 
 	//	gotEdges := g.Edges()
-	m := MakeConflictGraphOutOfConnectionGraph2(g)
+	m := MakeConflictGraphOutOfConnectionGraph(g)
 	gotEdges := m.UGraph.UEdges()
 
 	expected := []Edge{{2, 8},
@@ -480,7 +241,7 @@ func TestMakeConflictGraphOutOfConnectionGraph_2_6(t *testing.T) {
 	}
 
 	//	gotEdges := g.Edges()
-	m := MakeConflictGraphOutOfConnectionGraph2(g)
+	m := MakeConflictGraphOutOfConnectionGraph(g)
 	gotEdges := m.UGraph.UEdges()
 
 	expected := []Edge{{1, 4},
@@ -532,7 +293,7 @@ func TestMakeConflictGraphOutOfConnectionGraph_2_7(t *testing.T) {
 	}
 
 	//	gotEdges := g.Edges()
-	m := MakeConflictGraphOutOfConnectionGraph2(g)
+	m := MakeConflictGraphOutOfConnectionGraph(g)
 	gotEdges := m.UGraph.UEdges()
 
 	expected := []Edge{{1, 4},
@@ -551,6 +312,36 @@ func TestMakeConflictGraphOutOfConnectionGraph_2_7(t *testing.T) {
 		{16, 19},
 		{19, 16},
 		{20, 13}}
+
+	for index, element := range gotEdges {
+		if expected[index] != element {
+			t.Errorf("Wrong Node")
+		}
+	}
+}
+
+func TestMakeConflictGraphOutOfConnectionGraph4(t *testing.T) {
+	//Ein Linksabbieger und ein Geradeausfahrer -  die Spuren Kreuzen sich
+	// A, N
+
+	wantedEdges1 := []Edge{
+		{1, 4},
+		{4, 2},
+	}
+
+	g := NewGraphAdjMat(6)
+	for _, edge := range wantedEdges1 {
+		g.AddEdge(edge.from, edge.to)
+	}
+
+	//	gotEdges := g.Edges()
+	m := MakeConflictGraphOutOfConnectionGraph(g)
+	gotEdges := m.UGraph.UEdges()
+
+	expected := []Edge{
+		{3, 17},
+		{17, 3},
+	}
 
 	for index, element := range gotEdges {
 		if expected[index] != element {

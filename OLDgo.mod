9fa49ef5caf64bf896db7925378f72e762b40b0a
mike <lauterba@hm.edu>
2019-06-09 18:08:01 +0200

    call of welsh  powell implemented

---
+++ OLDgo.mod
@@ -0,0 +1,5 @@
+module github.com/ob-algdatii-ss19/leistungsnachweis-ateam
+
+go 1.12
+
+// +heroku goVersion go1.12
---
+++ backend/algorithms/welshPowell.go
@@ -0,0 +1,15 @@
+package algorithms
+
+import (
+	"fmt"
+	"github.com/ob-algdatii-ss19/leistungsnachweis-ateam/backend/adjGraph"
+)
+
+/*
+Calculate the optimization of the traffic lights with basic Greedy algorithm
+*/
+func WelshPowell(graphData adjGraph.UGraph) [][]adjGraph.Node {
+	fmt.Println("[INFO] Called welshPowell Algorithm")
+
+	return nil
+}

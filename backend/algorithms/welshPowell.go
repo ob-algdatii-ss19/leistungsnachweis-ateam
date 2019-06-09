f42aae280216e555789a75d46a75f2c9b93dc525
mike <lauterba@hm.edu>
2019-06-09 22:24:33 +0200

    first sorting

--- backend/algorithms/welshPowell.go
+++ backend/algorithms/welshPowell.go
@@ -11,5 +11,46 @@ Calculate the optimization of the traffic lights with basic Greedy algorithm
 func WelshPowell(graphData adjGraph.UGraph) [][]adjGraph.Node {
 	fmt.Println("[INFO] Called welshPowell Algorithm")
 
+
+
 	return nil
+}
+
+
+
+//make graph to []
+//call sortNodesDescending
+
+//loop over all nodes to find all nodes where the cars can drive simultaneously
+	//starts with node with most edges
+
+
+//returns a [][]
+	//columns: all nodes
+	//rows: nodes where cars can drive simultaneously
+func giveSameColor(graphData adjGraph.UGraph) [][]adjGraph.Node{
+
+
+	return nil
+
+}
+
+
+//this function make the first step and order the nodes descending
+//node with most edges is first, with less edges is last
+func sortNodesDescending (graphArray []adjGraph.Node) []adjGraph.Node{
+	returnGraphArray := graphArray; //copy
+
+	for i := 0; i < len(graphArray); i++{
+		actElementOuter :=graphArray[i];
+		for j := (i+1); j <  len(graphArray); j++{
+		actElementInner :=graphArray[j];
+			if(actElementInner>actElementOuter){ //swap
+				returnGraphArray[i]=actElementInner;
+				returnGraphArray[j]=actElementOuter;
+			}
+		}
+	}
+
+	return returnGraphArray
 }
